ab466d2cd6b84e5be7d0215d70bb33bcdf7ea23d
theikeofficial <ajzenhamernikola@gmail.com>
2016-09-23 18:35:50 +0200

    Added new functionality.
    Added number that represents Node label.
    Made Node movable.

--- node.cpp
+++ node.cpp
@@ -1,6 +1,22 @@
 #include "node.h"
 
-Node::Node()
+Node::Node(int value)
+    :m_value(value)
 {
+    setFlag(GraphicsItemFlag::ItemIsMovable);
+}
 
+QRectF Node::boundingRect() const
+{
+    qreal penWidth = 1;
+    return QRectF(-10 - penWidth/2, -10 - penWidth/2,
+                  20 + penWidth, 20 + penWidth);
+}
+
+void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    (void)option;
+    (void)widget;
+    painter->drawEllipse(boundingRect());
+    painter->drawText(-3, 4, QString::number(m_value));
 }
--- node.h
+++ node.h
@@ -1,11 +1,20 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <QtGui>
+#include <QtCore>
+#include <QGraphicsItem>
 
 class Node : public QGraphicsItem
 {
 public:
-    Node();
+    explicit Node(int value);
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = Q_NULLPTR);
+
+private:
+    int m_value;
 };
 
 #endif // NODE_H

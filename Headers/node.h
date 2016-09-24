14a4249916cc03edd3d48d824c597b105fa8bab8
theikeofficial <ajzenhamernikola@gmail.com>
2016-09-24 12:50:02 +0200

    Changed base class, added new signals, and added new mutators and operator.
    Changed base class from QGraphicsItem to QGraphicsObject, since the Node
    uses signals framework.
    Added signals "activated" and "deactivated".
    Added mutators "value" and "deactivate".
    Added operator==.

--- Headers/node.h
+++ Headers/node.h
@@ -1,21 +1,30 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <QGraphicsObject>
 #include <QtGui>
-#include <QtCore>
-#include <QGraphicsItem>
 
-class Node : public QGraphicsItem
+class Node : public QGraphicsObject
 {
+    Q_OBJECT
+
 public:
     explicit Node(int value);
 
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = Q_NULLPTR);
 
+    int value() const;
+    bool operator==(const Node& other) const;
+
+    void deactivate();
+
+signals:
+    void activated(Node* node);
+    void deactivated(Node* node);
+
 protected:
-    void mousePressEvent(QGraphicsSceneMouseEvent *event);
-    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
 
 private:
     int m_value;

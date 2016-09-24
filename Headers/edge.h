2360a70e12da113e59e6783ef06d114d7a18d0cb
theikeofficial <ajzenhamernikola@gmail.com>
2016-09-24 13:52:33 +0200

    Added new mutators.
    Addded "value" and "setValue" for getting and setting an Edge value.
    Added "nodes" for getting a pair of values of the Nodes that are
    adjacent to this Edge.

--- Headers/edge.h
+++ Headers/edge.h
@@ -1,11 +1,26 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include <QGraphicsObject>
+#include <QtGui>
+#include <QPair>
+
+#include "../Headers/node.h"
 
 class Edge : public QGraphicsObject
 {
 public:
-    Edge();
+    Edge(Node *from, Node *to, int value = 1);
+
+    int value() const;
+    QPair<int, int> nodes() const;
+
+    void setValue(int value);
+
+private:
+    Node *m_from;
+    Node *m_to;
+    int m_value;
 };
 
 #endif // EDGE_H
--- Sources/edge.cpp
+++ Sources/edge.cpp
@@ -1,6 +1,24 @@
-#include "edge.h"
+#include "../Headers/edge.h"
 
-Edge::Edge()
+// public
+
+Edge::Edge(Node *from, Node *to, int value)
+    : m_from(from), m_to(to), m_value(value)
 {
 
+}
+
+int Edge::value() const
+{
+    return m_value;
+}
+
+QPair<int, int> Edge::nodes() const
+{
+    return QPair<int, int>(m_from->value(), m_to->value());
+}
+
+void Edge::setValue(int value)
+{
+    m_value = value;
 }

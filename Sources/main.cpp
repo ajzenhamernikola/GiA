d5fef3c2247970a7025123363c6c8d07ad58b116
theikeofficial <ajzenhamernikola@gmail.com>
2016-10-05 14:30:59 +0200

    Refactored GUI styling using stylesheets.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,43 +1,21 @@
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 #include "Headers/mainmenu.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setStyleSheet(
-        QString(
-            "QPushButton {"
-                "background-color: #3399CC;"
-                "border: 0px solid black;"
-                "padding: 10px;"
-                "font-family: sans-serif;"
-                "font-size: 15px;"
-                "font-weight: bold;"
-                "color: white;"
-            "}"
-            " "
-            "QPushButton:pressed {"
-                "background-color: #2681A5;"
-            "}"
-            " "
-            "QLabel {"
-                "color: #3399CC;"
-                "font-family: sans-serif;"
-                "font-size: 15px;"
-            "}"
-            " "
-            "QCheckBox {"
-                "color: #3399CC;"
-                "font-family: sans-serif;"
-                "font-size: 15px;"
-            "}"
-            " "
-            "QGraphicsView {"
-                "border: 10px solid #3399CC;"
-            "}"
-        )
-    );
+
+    QFile stylesheetFile(":/css/application.css");
+    if(stylesheetFile.open(QFile::ReadOnly))
+    {
+        QTextStream stylesheet(&stylesheetFile);
+        a.setStyleSheet(stylesheet.readAll());
+    }
+
     MainMenu w;
     w.show();
 
